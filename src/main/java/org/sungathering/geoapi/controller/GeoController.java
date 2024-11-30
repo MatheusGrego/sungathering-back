2549f73be4ce043f3c4f2f05ef2a035ede52271c
matheus.grego <matheusdeleongrego@gmail.com>
2024-11-29 22:18:05 -0300

    fix:cors

--- src/main/java/org/sungathering/geoapi/controller/GeoController.java
+++ src/main/java/org/sungathering/geoapi/controller/GeoController.java
@@ -2,15 +2,13 @@ package org.sungathering.geoapi.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.sungathering.geoapi.model.MarkersInputDTO;
 import org.sungathering.geoapi.service.GeoService;
 
 @RestController
 @RequestMapping("/api/geo")
+@CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET}, allowedHeaders = "*")
 public class GeoController {
 
     private final GeoService geoService;
